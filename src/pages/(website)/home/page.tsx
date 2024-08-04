5ef6f3e42ea32c3cfa5cc5f2f0efd06a5c6a03a6
luongtu2004 <hoanghai962002@gmail.com>
2024-08-04 22:54:10 +0700

    https://github.com/luongtu2004/DuAnTN.git

---
+++ src/pages/(website)/home/page.tsx
@@ -0,0 +1,14 @@
+
+import AboutPage from './_components/AboutPage'
+
+type Props = {}
+
+const HomePage = (props: Props) => {
+  return (
+    <div>
+      <AboutPage />
+    </div>
+  )
+}
+
+export default HomePage
